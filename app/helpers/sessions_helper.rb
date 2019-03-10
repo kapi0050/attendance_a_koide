5abe7a3a8403d57395e6e0629b7aa6354a28eb32
koideteruhiko <kapi0050@gmail.com>
2019-03-10 19:26:32 +0000

    7章おわり

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,11 @@ module SessionsHelper
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
+  
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
@@ -21,6 +26,17 @@ module SessionsHelper
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  # 記憶しているURL (もしくはデフォルト値) にリダイレクトする
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを記憶しておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
   
 end
